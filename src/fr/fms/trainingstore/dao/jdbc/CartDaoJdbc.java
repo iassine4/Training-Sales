b58a938e2bf069344b9f2a09d7a6318f4449f5a3
Yassine <yassine.zriba@fms-ea.com>
2026-01-09 15:05:23 +0100

    feat: initial implement of the jdbc cart dao

---
+++ src/fr/fms/trainingstore/dao/jdbc/CartDaoJdbc.java
@@ -0,0 +1,45 @@
+/**
+ * 
+ */
+package fr.fms.trainingstore.dao.jdbc;
+
+import fr.fms.trainingstore.dao.CartDao;
+import fr.fms.trainingstore.domain.Cart;
+
+/**
+ * @author ZribaY
+ *
+ */
+public class CartDaoJdbc implements CartDao {
+
+	@Override
+	public Cart findOrCreateByUserId(int userId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Cart findByUserId(int userId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean addTraining(int userId, int trainingId) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean removeOneTraining(int userId, int trainingId) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean clearCart(int userId) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
