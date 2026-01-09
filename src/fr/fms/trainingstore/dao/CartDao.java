609e3b2a2aeb00c67591e7964450761e1665bb2a
Yassine <yassine.zriba@fms-ea.com>
2026-01-09 14:40:41 +0100

    feat: add cart dao interface

--- src/fr/fms/trainingstore/dao/CartDao.java
+++ src/fr/fms/trainingstore/dao/CartDao.java
@@ -3,10 +3,21 @@
  */
 package fr.fms.trainingstore.dao;
 
+import fr.fms.trainingstore.domain.Cart;
+
 /**
  * @author ZribaY
  *
  */
 public interface CartDao {
-	//TOO DO
+
+	Cart findOrCreateByUserId(int userId);
+
+    Cart findByUserId(int userId);
+
+    boolean addTraining(int userId, int trainingId);
+
+    boolean removeOneTraining(int userId, int trainingId);
+
+    boolean clearCart(int userId);
 }
